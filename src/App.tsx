2c37e63ba5d2bc9324cfa6134df36231f1eb6682
Julien Jeanneret <julien.jeanneret@outlook.com>
2022-11-11 20:30:35 +0100

    feat: add env and make a simple project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [date, setDate] = useState(new Date());
+    useEffect(() => {
+        fetch(process.env.REACT_APP_BACKEND_URL + '/wealth')
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
+    return (
+        <div>
+            <form>
+                <label>Date:</label>
+                <input type="date" name="date"/>
+                <input type="submit" value="Submit"/>
+            </form>
+        </div>
+    );
+};
 
 export default App;
