1e8a88e8353141ba0207f665236581c3beb03b42
Julien Jeanneret <julien.jeanneret@outlook.com>
2023-04-23 19:24:41 +0200

    feat: make wealth

--- src/components/page-layout.tsx
+++ src/components/page-layout.tsx
@@ -2,22 +2,40 @@ import {Outlet} from "react-router";
 import React from "react";
 import Link from "@mui/material/Link";
 import Box from "@mui/material/Box";
+import {createTheme, ThemeProvider} from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
 
 const PageLayout = () => {
     return (
-        <Box>
-            <Box sx={{
-                typography: 'body1',
-                ml: 2
-            }}>
-                <Link href={`wealth`}>Wealth</Link>
-                <Link href={`balances`}>Balance</Link>
-                <Link href={`accounts`}>Accounts</Link>
-            </Box>
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline/>
+
             <Box>
-                <Outlet/>
+                <Box sx={{
+                    display: 'flex',
+                    backgroundColor: 'primary',
+                    '& > a': {
+                        padding: darkTheme.spacing(1)
+                    },
+
+                }}>
+                    <Link href={`wealth`}>Wealth</Link>
+                    <Link href={`balances`}>Balance</Link>
+                    <Link href={`accounts`}>Accounts</Link>
+                </Box>
+                <Box sx={{
+                    m: darkTheme.spacing(1)
+                }}>
+                    <Outlet/>
+                </Box>
             </Box>
-        </Box>
+        </ThemeProvider>
     );
 };
 
--- src/components/show-wealth.tsx
+++ src/components/show-wealth.tsx
@@ -1,10 +1,20 @@
 import React, {useEffect, useState} from "react";
 import {getWealth, WealthDto} from "../service";
 import {useMsal} from "@azure/msal-react";
+import {DatePicker} from "@mui/x-date-pickers/DatePicker";
+import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
+import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs, {Dayjs} from "dayjs";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import {AccordionDetails} from "@mui/material";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const ShowWealth = () => {
-    const [date, setDate] = useState(new Date().toISOString().split('T')[0]);
-    const { instance } = useMsal();
+    const [date, setDate] = useState<Dayjs | null>(dayjs());
+    const {instance} = useMsal();
     const [wealth, setWealth] = useState<WealthDto>();
     useEffect(() => {
         if (!!date) {
@@ -20,35 +30,47 @@ const ShowWealth = () => {
         }
     }, [date, instance]);
 
-
     const renderWealth = () => {
-        const emptyDetails = (<div>There is no details</div>);
+        const emptyDetails = (<Typography>There is no details</Typography>);
         const details = wealth?.Details.length === 0 ? emptyDetails : wealth?.Details.map(d =>
             (
-                <div key={d.AccountId}>
-                    <div>{d.AccountName} - {d.AccountCompany}</div>
-                    <div>{d.AmountInChf}</div>
-                    <div>{d.CheckDate}</div>
-                </div>
+                <Accordion key={d.AccountId}>
+                    <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+                        <Typography>{d.AccountName} - {d.AmountInChf} CHF</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography>
+                            The account {d.AccountName} by {d.AccountCompany} had {d.AmountInChf} CHF on {d.CheckDate}
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
             ));
         return (
-            <div>
-                <div>My wealth for {wealth?.ExportDate} is {wealth?.AmountInChf} CHF.</div>
-                <div>{details}</div>
-            </div>
+            <Box>
+                <Typography>
+                    The wealth at {wealth?.ExportDate} is {wealth?.AmountInChf} CHF.
+                </Typography>
+                {details}
+            </Box>
         );
     };
 
     return (
-        <div>
+        <Box>
+            <h1>Get you actual wealth</h1>
             <form>
-                <label>Date:</label>
-                <input type="date" name="date" value={date} onChange={(event) => setDate(event.target.value)}/>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                        label="Wealth at"
+                        value={date}
+                        onChange={(newValue) => setDate(newValue)}
+                    />
+                </LocalizationProvider>
             </form>
             <div>
                 {renderWealth()}
             </div>
-        </div>
+        </Box>
     );
 };
 
